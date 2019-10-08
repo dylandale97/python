053e3f0d9dfe9c5dd8beac37e246cc2606bf824c
Dylan Dale <dylandale97@hotmail.com>
2019-10-08 23:03:26 +0200

    stop

---
+++ StopWatch.py
@@ -0,0 +1,29 @@
+class StopWatch():
+    startTime = 0
+    endTime = 0
+
+    def __init__(self, start_time):
+        self.startTime = start_time
+
+    def __init__():
+        startTime = 0
+
+    def start(st):
+        startTime = st
+    
+    def stop():
+        endTime = startTime
+
+    def get_elapsed_time():
+        return startTime - endTime
+    
+    def getStartTime(self):
+        return self.startTime
+
+    def getEndTime():
+        return 
+    
+
+
+
+
